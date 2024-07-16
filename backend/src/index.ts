9cc645e4d0e5b5c8ed5a03de3adc441d2c2b3bea
Litenoo <lukaszcholewka@outlook.com>
2024-07-17 00:01:45 +0200

    gitIgnore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,26 @@
-import express from "express";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import session from 'express-session';
+import winston from 'winston';
 
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send("Hi there !");
-})
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser);
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    saveUninitialized: true,
+    resave: false,
+    cookie: { secure: process.env.NODE_ENV === 'production', httpOnly: true } // Not sure what is this
+}));
 
 app.listen(3000, ()=>{
     console.log("Server is listening on 3000");
