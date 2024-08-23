653146774e3829d8120406a248fc3f4cd6d7f34d
Litenoo <lukaszcholewka@outlook.com>
2024-08-23 21:12:48 +0200

    Added separated Settings component

--- frontend/src/components/Lobby.vue
+++ frontend/src/components/Lobby.vue
@@ -1,7 +1,9 @@
 <script setup>
 import socket from '../socket'
 import { ref, computed } from "vue";
-import { useClipboard } from "@vueuse/core"
+import { useClipboard } from "@vueuse/core";
+
+import Settings from "./Settings.vue";
 
 //variables init :
 const alphabet = [
@@ -75,15 +77,6 @@ const nonHosts = computed(() => players.value.filter(player => player.isHost ===
                 <button @click="startGame()" class=" bg-zinc-900">Start Game</button>
             </div>
         </div>
-        <div class="flex flex-col pl-3">
-            Settings
-            <input type="text" placeholder="cattegory" class="m-1 p-0.5 rounded-md">
-            <input type="number" required placeholder="Time (in seconds)" max="600" class="m-1 p-0.5 rounded-md">
-            <div class="flex flex-wrap max-w-96">
-                <label for="letter" v-for="letter in alphabet">
-                    {{ letter }}<input type="checkbox" checked name="letter">
-                </label>
-            </div>
-        </div>
+        <Settings/>
     </div>
 </template>
---
+++ frontend/src/components/Settings.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col pl-3">
+        Settings
+        <input type="text" placeholder="cattegory" class="m-1 p-0.5 rounded-md">
+        <input type="number" required placeholder="Time (in seconds)" max="600" class="m-1 p-0.5 rounded-md">
+        <div class="flex flex-wrap max-w-96">
+            <label for="letter" v-for="letter in alphabet">
+                {{ letter }}<input type="checkbox" checked name="letter">
+            </label>
+        </div>
+    </div>
+</template>
